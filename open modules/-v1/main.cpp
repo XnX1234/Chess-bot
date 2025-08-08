3a6ffc273c9d54bda0173ad515717a7f50d4843a
XnX1234 <maksymilian.walczuk@gmail.com>
2025-08-08 22:50:59 +0200

    Update main.cpp

--- open modules/-v1/main.cpp
+++ open modules/-v1/main.cpp
@@ -1,1 +1,20 @@
+//=============================================================//
+//																														 //
+// 	Copyright (C) XnX1234 2025														 		 //
+//																														 //
+//	Thid file is under PolyForm Noncommercial License 1.0.0		 //
+//	<https://polyformproject.org/licenses/noncommercial/1.0.0> //
+//																														 //
+//	Go to LICENSE file to get more information								 //
+//																														 //
+//=============================================================//
+#include <vector>
 
+using namespace std;
+
+int main()
+{
+  //code/module
+  //Copyright (C) XnX1234 2025
+  //end
+}
